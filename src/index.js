4d99fef231ad58d823231a9fee8eddb5c6cfabfd
Nooshin0962 <nooha.0962@gmail.com>
2023-11-03 20:48:45 +0330

    Adding Api Search

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+function refreshWeather(response){
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+temperatureElement.innerHTML = Math.round(temperature);
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.city;
+}
+
+function serachCity(city){
+let apiKey = "b60097c3ddaff0a42bft015121o9bfe4";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+serachCity(searchInput.value);
 }
 
 
 let serachFormElement = document.querySelector("#search-form");
 serachFormElement.addEventListener("submit",handleSearchSubmit);
+serachCity("Rome");
 
 
