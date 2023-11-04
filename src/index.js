d05c0f617a734e848706aed235bfd97fffd487a7
Nooshin0962 <nooha.0962@gmail.com>
2023-11-04 12:27:54 +0330

    Injecting weather Data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ let temperature = response.data.temperature.current;
 temperatureElement.innerHTML = Math.round(temperature);
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = response.data.city;
+let descriptionElement = document.querySelector("#description");
+descriptionElement.innerHTML = response.data.condition.description;
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = response.data.temperature.humidity;
+let windElement = document.querySelector("#wind");
+windElement.innerHTML =  Math.round(response.data.wind.speed);
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
 }
 
 function serachCity(city){
